448fcabc628e94a5fbc4cf5131fa544478c48ca7
Aaron GT <denvernuggets32@gmail.com>
2025-02-24 02:23:57 -0500

    Made changes to deploy.yml and eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,10 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
-
-/** @type {import('eslint').Linter.Config[]} */
+import js from "@eslint/js";
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+  {
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+    },
+    files: ["*.js"],
+  },
 ];
